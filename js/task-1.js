f47b3a46dbcef329c492ae7c6757ef623ceb04e1
Roman Kniazhyk <romanknyazhyk@gmail.com>
2025-08-17 11:47:00 +0300

    End homework 4 tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,21 +1,19 @@
 'use strict';
 
-function slugify(title) {
-  let slug = '';
-  const lowerCasedTitle = title.toLowerCase();
-  const words = lowerCasedTitle.split(' ');
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].length > 0) {
-      slug += words[i];
-      if (i < words.length - 1) {
-        slug += '-';
-      }
-    }
+function isEnoughCapacity(products, containerSize) {
+  let productsSize = 0;
+  let arrayOfproperty = Object.values(products);
+
+  for (let product of arrayOfproperty) {
+    productsSize += product;
   }
-  return slug;
+  return productsSize <= containerSize;
 }
 
-console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
