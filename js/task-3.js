63478e09e72ce5296647ecda19776177e6129afb
Roman Kniazhyk <romanknyazhyk@gmail.com>
2025-08-17 10:41:23 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function filterArray(array, value) {
+  let arr1 = [];
+  for (const item of array) {
+    if (item > value) {
+      arr1.push(item);
+    }
+  }
+  return arr1;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
